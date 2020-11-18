fe1204ae7ed50b54606844d67801d66898fe5910
Guilherme Carra <gui_gc@hotmail.com>
2020-11-18 13:19:59 +0100

    Change: players and objects from const to var so it can modified an accessed by any func

--- 1.CastleBlack/src/api.js
+++ 1.CastleBlack/src/api.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const api = Router();
 
 // This will be your data source
-const players = [
+var players = [
   { id: 1, name: "Jon Snow", age: 23, health: 100, bag: [1] },
   { id: 2, name: "Littlefinger", age: 35, health: 100, bag: [2] },
   { id: 3, name: "Daenerys Targaryen", age: 20, health: 100, bag: [3] },
